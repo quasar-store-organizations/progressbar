8d47cbb535750de976d3bcd9cadca7fc7465c3cf
Quasar <91174081+quasar-scripts@users.noreply.github.com>
2023-11-19 03:58:29 +0100

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+lua54 'yes'
+
+game 'gta5'
+
+ui_page 'html/index.html'
+
+client_script 'client.lua'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
